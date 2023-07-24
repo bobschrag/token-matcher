3d4351d6d405fa9feb278c7d2efe7fcda52699aa
Robert Schrrag <bob.schrag@gmail.com>
2023-07-24 14:34:11 -0700

    Increment version.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject token-matcher "1.0"
+(defproject token-matcher "1.0.1"
   :description "Match a template containing variables against an input string, creating appropriate token-level bindings that a using application may process further."
   :url "https://github.com/bobschrag/token-matcher/"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
